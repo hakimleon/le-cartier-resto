994aef0cd759aa17ab7a3359620d09f5c41eb527
hakim leon <hakimleon4@gmail.com>
2025-08-21 00:37:05 +0000

    utilise combobox de shadcn

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -47,7 +47,7 @@ export function Combobox({ options, value, onSelect, placeholder, searchPlacehol
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0" align="start">
+      <PopoverContent className="w-[--radix-popover-trigger-width] p-0" align="start">
         <Command>
           <CommandInput placeholder={searchPlaceholder || "Search..."} />
            <CommandList>
