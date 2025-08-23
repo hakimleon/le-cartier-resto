f564d702ab959cfd896d6941d6a7363d6ecabcf2
hakim leon <hakimleon4@gmail.com>
2025-08-23 00:36:42 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -6,13 +5,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // IMPORTANT: This is a public-facing config object, and security is enforced via Firestore Security Rules.
 const firebaseConfig = {
-  projectId: "le-singulier-ai",
-  appId: "1:160628171290:web:332eeec20941d04cf9a7a3",
-  storageBucket: "le-singulier-ai.firebasestorage.app",
-  apiKey: "AIzaSyB11kYlG3sbDZj0yEBncQ9n7PHYO4EyCmg",
-  authDomain: "le-singulier-ai.firebaseapp.com",
-  messagingSenderId: "160628171290"
+  apiKey: "AIzaSyCuO62Uf-dGglqlrC1yFjICQkYYbvvv3us",
+  authDomain: "le-singulier-d4513.firebaseapp.com",
+  projectId: "le-singulier-d4513",
+  storageBucket: "le-singulier-d4513.firebasestorage.app",
+  messagingSenderId: "606165341069",
+  appId: "1:606165341069:web:1d70d57e873261645963c4",
+  measurementId: "G-RQM9MZCR5X"
 };
+
 
 // Initialize Firebase
 let app;
