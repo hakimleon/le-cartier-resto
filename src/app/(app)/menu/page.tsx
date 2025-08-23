ae68cb40cc5a2163e092c0f90776b4da5628483d
hakim leon <hakimleon4@gmail.com>
2025-08-23 00:56:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -1,23 +1,8 @@
 
 
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import { AppHeader } from "@/components/common/AppHeader";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Recipe, categories } from "@/data/definitions";
-import { Badge } from "@/components/ui/badge";
-import { cn } from "@/lib/utils";
-import { PlusCircle, Edit, Trash2, Clock, Star, FileText, Search } from "lucide-react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
-import { DishForm } from "./DishForm";
-import { useToast } from "@/hooks/use-toast";
-import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "@/lib/firebase";
+import { Recipe } from "@/data/definitions";
 import { MenuClient } from "./MenuClient";
 
 
