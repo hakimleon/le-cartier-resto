343e9292ca2110ce0f94d626a2b84e50ab4ff0bd
hakim leon <hakimleon4@gmail.com>
2025-08-25 17:29:00 +0000

    reduire la largeur de la siebar et mettre un padding a la page menu pour

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -3,8 +3,10 @@ import MenuClient from './MenuClient';
 
 export default function MenuPage() {
   return (
-    <div className="container py-10">
+    <div className="container py-10 px-8">
       <MenuClient />
     </div>
   );
 }
+
+    
