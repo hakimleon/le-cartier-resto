eb23d3aa12c0ca4a30efbe4b0a78069f826c4037
hakim leon <hakimleon4@gmail.com>
2025-08-28 21:22:34 +0000

    non, je parle de l'occupation de la page dans sa totale largeur

--- src/app/(app)/menu/[id]/page.tsx
+++ src/app/(app)/menu/[id]/page.tsx
@@ -3,7 +3,7 @@ import RecipeDetailClient from './RecipeDetailClient';
 
 export default function RecipeDetailPage({ params }: { params: { id: string } }) {
   return (
-    <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8 max-w-7xl">
+    <div className="py-10 px-4 sm:px-6 lg:px-8">
       <RecipeDetailClient recipeId={params.id} />
     </div>
   );
