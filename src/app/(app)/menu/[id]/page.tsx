1239a841609ffd3340739daad3aa19c51c5b1fb2
hakim leon <hakimleon4@gmail.com>
2025-08-28 21:18:15 +0000

    la fenêtre de la fiche technique  n'occupe pas toute la largeure la page

--- src/app/(app)/menu/[id]/page.tsx
+++ src/app/(app)/menu/[id]/page.tsx
@@ -3,7 +3,7 @@ import RecipeDetailClient from './RecipeDetailClient';
 
 export default function RecipeDetailPage({ params }: { params: { id: string } }) {
   return (
-    <div className="container mx-auto py-10 px-8">
+    <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8 max-w-7xl">
       <RecipeDetailClient recipeId={params.id} />
     </div>
   );
