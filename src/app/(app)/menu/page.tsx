4ab6df1788f1252c23946aaa4d0065a1bccca0c4
hakim leon <hakimleon4@gmail.com>
2025-09-08 21:46:23 +0000

    tu peux elargir le conteneur de la page de menu ?

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -3,10 +3,8 @@ import MenuClient from './MenuClient';
 
 export default function MenuPage() {
   return (
-    <div className="container py-10 px-8">
+    <div className="w-full py-10 px-8">
       <MenuClient />
     </div>
   );
 }
-
-    
