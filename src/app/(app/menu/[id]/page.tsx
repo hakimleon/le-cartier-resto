d9b49ee1088f3a0fb589b12b3c876a307f94179a
hakim leon <hakimleon4@gmail.com>
2025-09-09 18:44:07 +0000

    non , toujours pas elargie pour /assistant

---
+++ src/app/(app/menu/[id]/page.tsx
@@ -0,0 +1,14 @@
+
+import RecipeDetailClient from './RecipeDetailClient';
+
+// Remove searchParams as they are no longer needed for this flow
+export default async function RecipeDetailPage({ params }: { params: { id: string } }) {
+  
+  return (
+    <div className="flex-1 overflow-y-auto">
+      <div className="py-10">
+        <RecipeDetailClient recipeId={params.id} />
+      </div>
+    </div>
+  );
+}
