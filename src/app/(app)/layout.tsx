bbb7bff2166017ac23ae76b41800d182b8a295cd
hakim leon <hakimleon4@gmail.com>
2025-09-18 22:51:44 +0000

    avant de tester tous ca tu peux ameliorer le menu du side bar je soushai

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -15,6 +15,7 @@ import {
   SidebarMenuButton,
   SidebarInset,
   SidebarTrigger,
+  SidebarSeparator,
 } from '@/components/ui/sidebar';
 import { Button } from '@/components/ui/button';
 import { NotebookText, Carrot, LayoutDashboard, Settings, ChefHat, FlaskConical, CookingPot } from 'lucide-react';
@@ -68,87 +69,75 @@ export default function AppLayout({
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+              
+              <SidebarSeparator className="my-2" />
+
+              {/* Groupe Plats */}
               <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/menu')}
-                  tooltip="Menu"
-                >
+                  <SidebarMenuButton asChild isActive={pathname.startsWith('/workshop')} tooltip="Atelier Plats">
+                    <Link href="/workshop">
+                      <FlaskConical />
+                      <span>Atelier Plats</span>
+                    </Link>
+                  </SidebarMenuButton>
+              </SidebarMenuItem>
+               <SidebarMenuItem>
+                <SidebarMenuButton asChild isActive={pathname.startsWith('/menu')} tooltip="Menu">
                   <Link href="/menu">
                     <ChefHat />
-                    <span>Menu</span>
+                    <span>Menu & Plats</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+
+              <SidebarSeparator className="my-2" />
+
+              {/* Groupe Préparations */}
+              <SidebarMenuItem>
+                  <SidebarMenuButton asChild isActive={pathname.startsWith('/preparations/workshop')} tooltip="Atelier Préparations">
+                    <Link href="/preparations/workshop">
+                      <FlaskConical />
+                      <span>Atelier Préparations</span>
+                    </Link>
+                  </SidebarMenuButton>
+              </SidebarMenuItem>
                <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/preparations-base')}
-                  tooltip="Préparations"
-                >
+                <SidebarMenuButton asChild isActive={pathname.startsWith('/preparations-base')} tooltip="Préparations de Base">
                   <Link href="/preparations-base">
                     <NotebookText />
                     <span>Préparations</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+              
+              <SidebarSeparator className="my-2" />
+
+              {/* Groupe Garnitures */}
+               <SidebarMenuItem>
+                <SidebarMenuButton asChild isActive={pathname.startsWith('/garnishes/workshop')} tooltip="Atelier Garnitures">
+                  <Link href="/garnishes/workshop">
+                    <FlaskConical />
+                    <span>Atelier Garnitures</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
               <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/garnishes') && !pathname.includes('workshop')}
-                  tooltip="Garnitures"
-                >
+                <SidebarMenuButton asChild isActive={pathname.startsWith('/garnishes') && !pathname.includes('workshop')} tooltip="Garnitures">
                   <Link href="/garnishes">
                     <CookingPot />
                     <span>Garnitures</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+             
+              <SidebarSeparator className="my-2" />
+
+              {/* Section Ingrédients */}
               <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/ingredients')}
-                  tooltip="Ingrédients"
-                >
+                <SidebarMenuButton asChild isActive={pathname.startsWith('/ingredients')} tooltip="Ingrédients">
                   <Link href="/ingredients">
                     <Carrot />
                     <span>Ingrédients</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-                <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/workshop')}
-                  tooltip="Atelier Plats"
-                >
-                  <Link href="/workshop">
-                    <FlaskConical />
-                    <span>Atelier Plats</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-               <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/preparations/workshop')}
-                  tooltip="Atelier Préparations"
-                >
-                  <Link href="/preparations/workshop">
-                    <FlaskConical />
-                    <span>Atelier Préparations</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname.startsWith('/garnishes/workshop')}
-                  tooltip="Atelier Garnitures"
-                >
-                  <Link href="/garnishes/workshop">
-                    <FlaskConical />
-                    <span>Atelier Garnitures</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
